cd71d38a4d7f32478d5a7bb8e67cb49c418415ab
ShriramShagri <shagrisrm@gmail.com>
2020-10-23 23:12:55 +0530

    Convertor skeleton code

---
+++ src/convertor.py
@@ -0,0 +1,18 @@
+class Converter():
+    '''
+    Format converter
+    '''
+    def __init__(self, *args, **kwargs):
+        pass
+
+    def audio(self, filepath, format, metadata, callback=None):
+        # Add metadata to song and convert to the format
+        pass
+
+    def videoWithAudio(self, filepath, format, metadata, callback=None):
+        # Add metadata to video and convert to the format
+        pass
+
+    def videoWithoutAudio(self, filepath, format, metadata, callback=None):
+        # Add metadata to video and convert to the formats
+        pass
